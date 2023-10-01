290c23bf1b9c915ddab743e2ff5b75bbb34a2581
Md. Masud Rana <mr.hasib45@gmail.com>
2023-10-01 12:36:50 +0600

    added Convert any value to boolean

---
+++ JS-Pro-Tips/Convert-to-boolean.js
@@ -0,0 +1,10 @@
+// convert any value to boolean
+
+// Using !! in front of any value it will convert the value to boolean
+console.log(!!"Hasib"); //true
+console.log(!!1); //true
+console.log(!!0); //false
+console.log(!!undefined); //false
+
+// we can also use Boolean() to achieve same
+console.log(Boolean("Ayan")) //true
