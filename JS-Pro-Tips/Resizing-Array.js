1a767c4f751702ae069394fd0f42474d0017678f
Md. Masud Rana <mr.hasib45@gmail.com>
2023-10-02 23:51:13 +0600

    Resizing any array

---
+++ JS-Pro-Tips/Resizing-Array.js
@@ -0,0 +1,8 @@
+// Resizing any array
+
+let animals = ["Cat","Dog","Manky","Lion"];
+
+// we can use array's length property 
+animals.length= 3;
+
+console.log(animals); //[ 'Cat', 'Dog', 'Manky' ]
